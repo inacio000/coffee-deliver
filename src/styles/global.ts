1f3072a11de5ba1ce934ffb089415ed4da7b3851
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2023-06-13 16:26:14 +0300

    Refactored global style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -45,7 +45,6 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
-        font-family: 'Baloo 2', cursive;
     }
 
     html {
